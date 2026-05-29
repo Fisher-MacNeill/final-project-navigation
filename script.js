c41dac463caca62c1236fb538dc9e5dde1a3adc6
macneillfisher-cloud <encryptorjr2@gmail.com>
2026-05-29 09:06:38 -0300

    Attempt 1: QR Code Scanner

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 document.querySelector(".scan");
 
-function scan() {
-  navigator.mediaDevices.getUserMedia(video);
-}
+document.querySelector(".scan").addEventListener("click", setupCamera);
 
-document.querySelector(".scan").addEventListener("click", scan);
+const constraints = { video: true, audio: true };
 
-async function startMedia() {
-  // Define what media types you want to request
-  const constraints = {
-    video: true,
+// Camera setup function - returns a Promise so we have to call it in an async function
+async function setupCamera() {
+  // Find the video element on our HTML page
+  video = document.getElementById("video");
+
+  // Request the front-facing camera of the device
+  const stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
-  };
+    video: {
+      facingMode: "user",
+      height: { ideal: 1920 },
+      width: { ideal: 1920 },
+    },
+  });
+  video.srcObject = stream;
 
-  try {
-    // Request permission and get the stream
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-
-    // Find your HTML video element and link the stream
-    const videoElement = document.getElementById("webcamPreview");
-    videoElement.srcObject = stream;
-  } catch (error) {
-    // Handle permissions denial or missing hardware
-    console.error("Error accessing media devices:", error);
-  }
+  // Handle the video stream once it loads.
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
 }
-
-// Call the function (usually triggered by a button click event)
-startMedia();
 
 // WE NEED TO GET THIS TO WORK TOMORROW OR SCRAP THE IDEA COMPLETELY AND MOVE TO NUMBAS
